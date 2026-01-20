2668907af377f3dc28622752690e55d357db3ccb
realtimesolutionaios-ui <realtimesolution.aios@gmail.com>
2026-01-21 00:01:24 +0900

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,7 @@
+module.exports = function handler(req, res) {
+  res.status(200).json({
+    supabaseUrl: process.env.VITE_SUPABASE_URL,
+    supabaseAnonKey: process.env.VITE_SUPABASE_ANON_KEY,
+    gatewayUrl: process.env.VITE_GATEWAY_URL
+  });
+};
